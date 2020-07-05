a4b08049d88652007a3acd81234acf1080c394c4
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-07-05 02:57:15 +0200

    Add lab 3

--- AirMonitor/AirMonitor/Views/DetailsPage.xaml.cs
+++ AirMonitor/AirMonitor/Views/DetailsPage.xaml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirMonitor.Models;
+using AirMonitor.ViewModels;
 using Xamarin.Forms;
 
 namespace AirMonitor.Views
@@ -13,9 +15,12 @@ namespace AirMonitor.Views
     [DesignTimeVisible(false)]
     public partial class DetailsPage : ContentPage
     {
-        public DetailsPage()
+        public DetailsPage(Measurement item)
         {
             InitializeComponent();
+
+            var vm = BindingContext as DetailsViewModel;
+            vm.Item = item;
         }
 
         private void Help_Clicked(object sender, EventArgs e)
